706ce7189f0a9ff41c1291d611ec4b8facb3ea2a
Ricky Normandeau <ricky.normandeau@pnc.com>
2017-11-14 19:31:06 -0500

    add quiz json

---
+++ VirtualWalletUI/source/javascripts/sample-quiz-json.js
@@ -0,0 +1,37 @@
+{
+"questionItem": [
+ { "id":"1",
+  "questionText":"This is the first question of the quiz.",
+  "responseCorrect":"You answered the question correctly!",
+  "responseIncorrect":"You must be a fool.",
+  "answer": [
+   { "answerText":"Option A", "valid":"false"}
+   { "answerText":"Option B", "valid":"false"}
+   { "answerText":"Option C", "valid":"true"}
+   { "answerText":"Option D", "valid":"false"}
+  ]
+ }
+ { "id":"2",
+  "questionText":"This is the second question of the quiz.",
+  "responseCorrect":"Great job!",
+  "responseIncorrect":"You're a loser.",
+  "answer": [
+   { "answerText":"Option A", "valid":"false"}
+   { "answerText":"Option B", "valid":"true"}
+   { "answerText":"Option C", "valid":"false"}
+   { "answerText":"Option D", "valid":"false"}
+  ]
+ }
+ { "id":"3",
+  "questionText":"This is the third question of the quiz.",
+  "responseCorrect":"You are a security wizard!",
+  "responseIncorrect":"You have probably already been hacked.",
+  "answer": [
+   { "answerText":"Option A", "valid":"true"}
+   { "answerText":"Option B", "valid":"false"}
+   { "answerText":"Option C", "valid":"false"}
+   { "answerText":"Option D", "valid":"false"}
+  ]
+ }
+]
+}
--- VirtualWalletUI/source/javascripts/security-widget.js
+++ VirtualWalletUI/source/javascripts/security-widget.js
@@ -1,1 +1,30 @@
 console.log("Gains");
+
+
+$(document).ready(function(){
+
+  var pig = 0;
+
+  var nextPig = function(){
+    if (pig === 3) {
+      pig =1
+    } else {
+      pig = pig+1;
+    }
+    console.log("pig",pig);
+    $(".sq__pig-group img").hide();
+    $(".sq__pig-group img:nth-child("+pig+")").show();
+  }
+
+
+  
+  // initial functions
+  nextPig();
+
+
+  // buttons
+  $(".sq-btn-next").click( function() {
+    nextPig();
+  } )
+
+})
